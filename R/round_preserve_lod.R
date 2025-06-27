311a7eeb8897c92deed603faa52c638579d28be4
Satoyoshi-ISHIZAKI <ishizaki-satoyoshi766@g.ecc.u-tokyo.ac.jp>
2025-06-27 17:29:30 +0900

    Finished shaping the revised BVOC data

--- R/round_preserve_lod.R
+++ R/round_preserve_lod.R
@@ -1,10 +1,15 @@
 # Custom function to round numeric values while preserving "<LOD"
 round_preserve_lod <- function(x, digits = 3) {
-  # Check if the value is "<LOD"
-  if (x == "<LOD") {
+  #Check if the value is NA
+  if (is.na(x)) {
     return(x)
-  } else {
-    # Convert to numeric and round
-    return(as.character(round(as.numeric(x), digits)))
-  }
+    } else {
+    # Check if the value is "<LOD"
+    if (x == "<LOD") {
+      return(x)
+      } else {
+        # Convert to numeric and round
+        return(as.character(round(as.numeric(x), digits)))
+        }
+    }
 }
